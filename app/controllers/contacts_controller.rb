60f72b9de92d0889719a93dccd67d370b3d7e0b8
takayuki niinuma <>
2022-07-20 03:14:21 +0900

    a

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ class ContactsController < ApplicationController
   def create
     puts contact_params
     contact = Contact.create(contact_params)
+    notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
     user_type =
         if contact.user_type == 0
           "法人"
@@ -27,15 +28,8 @@ class ContactsController < ApplicationController
     市区町村: #{contact.city}
     具体的な内容: #{contact.body}
     EOS
+    notifier.ping(msg)
 
-    from = SendGrid::Email.new(email: 'nonbirin09@gmail.com')
-    to = SendGrid::Email.new(email: 'niinuma@totop.jp')
-    # minnzei@blueleaf-partners.co.jp
-    subject = 'メッセージが届きました'
-    content = SendGrid::Content.new(type: 'text/plain', value: msg)
-    mail = SendGrid::Mail.new(from, subject, to, content)
-    sg = SendGrid::API.new(api_key: ENV['SG_API_KEY'])
-    sg.client.mail._('send').post(request_body: mail.to_json)
     redirect_to thanks_path
   end
 
