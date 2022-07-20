e01ebbf4a4e4e5d11711d006fcdbdaa128ef9f19
takayuki niinuma <>
2022-07-20 09:28:47 +0900

    a

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -35,7 +35,9 @@ class ContactsController < ApplicationController
     mail = SendGrid::Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_APIKEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    logger.debug(response)
+    logger.debug(response.status_code)
+    logger.debug(response.body)
+    logger.debug(response.headers)
     redirect_to thanks_path
   end
 
